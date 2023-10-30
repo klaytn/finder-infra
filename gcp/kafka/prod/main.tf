74415c82c3bad05713aa4df7e0fa612df036cb86
jayce <jayce@bisonai.com>
2023-10-30 15:43:14 +0900

    fix: remove management vm for kafka

--- gcp/kafka/prod/main.tf
+++ gcp/kafka/prod/main.tf
@@ -14,10 +14,3 @@ module "zookeeper" {
   subnetwork        = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets_self_links[0]
   servers           = 3
 }
-
-module "management" {
-  source            = "../../modules/management"
-  project           = data.terraform_remote_state.foundation.outputs.vpc_finder_name.project_id
-  zone              = "${data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets_regions[0]}-a"
-  subnetwork        = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets_self_links[0]
-}
