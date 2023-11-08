3446f5f889a187487bf6abb491a8d1b8df697fc5
jayce <jayce@bisonai.com>
2023-11-08 10:02:44 +0900

    fix: update firewall rules

--- gcp/foundation/firewall.tf
+++ gcp/foundation/firewall.tf
@@ -77,5 +77,42 @@ module "firewall_rules_prod" {
         log_config = {
             metadata = "INCLUDE_ALL_METADATA"
         }
+    },
+    {
+        name                    = "allow-kafka"
+        description             = "Allow ingress for TCP"
+        direction               = "INGRESS"
+        priority                = 1000
+        ranges                  = ["10.178.0.0/24", "10.178.1.0/24", "10.178.2.0/24"]
+        source_tags             = null
+        source_service_accounts = null
+        target_tags             = null
+        target_service_accounts = null
+        allow = [{
+            protocol = "tcp"
+            ports    = ["2181", "2888", "3888", "9092"]
+        }]
+        deny = []
+        log_config = {
+            metadata = "INCLUDE_ALL_METADATA"
+        }
+    },
+    {
+        name                    = "allow-dataproc"
+        description             = "Allow ingress for TCP"
+        direction               = "INGRESS"
+        priority                = 1000
+        ranges                  = ["10.178.0.0/24", "10.178.1.0/24", "10.178.2.0/24"]
+        source_tags             = null
+        source_service_accounts = null
+        target_tags             = ["finder-streaming-prod"]
+        target_service_accounts = null
+        allow = [{
+            protocol = "all"
+        }]
+        deny = []
+        log_config = {
+            metadata = "INCLUDE_ALL_METADATA"
+        }
     }]
 }
