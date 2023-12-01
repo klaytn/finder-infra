ed5b7354fcf994b78580fac7962c11e93ac70c0d
jayce <jayce@bisonai.com>
2023-12-01 09:55:35 +0900

    fix: Update machine type and boot disk size in dataproc

--- gcp/dataproc/prod/main.tf
+++ gcp/dataproc/prod/main.tf
@@ -3,11 +3,11 @@ module "dataproc" {
     project                 = data.terraform_remote_state.foundation.outputs.vpc_finder_name.project_id
     subnetwork              = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets["asia-northeast3/sb-finder-prod-shared"].id
     region                  = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets_regions[0]
-    machine_type            = "e2-standard-8"
+    machine_type            = "e2-highmem-4"
     name                    = "finder-streaming-prod"
     tags                    = ["finder-streaming-prod"]
     image_version           = "2.0.82-debian10"
-    boot_disk_size_gb       = 128
+    boot_disk_size_gb       = 30
     master_num_instances    = 1
     worker_num_instances    = 10
 }
