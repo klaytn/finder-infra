e3e43a6e61852b653bf0b67ab6f0759ea5d02144
hamin <hamin@bisonai.com>
2024-02-13 14:47:57 +0900

    Update master and boot disk size in dataproc module

--- gcp/dataproc/prod/main.tf
+++ gcp/dataproc/prod/main.tf
@@ -3,12 +3,12 @@ module "dataproc" {
     project                 = data.terraform_remote_state.foundation.outputs.vpc_finder_name.project_id
     subnetwork              = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets["asia-northeast3/sb-finder-prod-shared"].id
     region                  = data.terraform_remote_state.foundation.outputs.vpc_finder_prod_subnets_regions[0]
-    master_machine_type     = "e2-standard-16"
+    master_machine_type     = "e2-highmem-16"
     worker_machine_type     = "e2-standard-4"
     name                    = "finder-streaming-prod"
     tags                    = ["finder-streaming-prod"]
     image_version           = "2.0.82-debian10"
-    boot_disk_size_gb       = 30
+    boot_disk_size_gb       = 130
     master_num_instances    = 1
     worker_num_instances    = 10
 }
